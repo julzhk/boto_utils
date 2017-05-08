ee77c893f149a021f670f4615ee825db5e1df696
julz <julian.harley@gmail.com>
2017-05-08 15:27:49 +0100

    update credentials parser for newer aws credentials format

--- boto_utils/common.py
+++ boto_utils/common.py
@@ -7,27 +7,36 @@ LINE_RE = re.compile(r'^\s*(.*?)=(.*?)\s*$')
 def parse_aws_credentials_file(fp):
     """Return a dictionary containing aws_access_key_id and aws_secret_access_key, given a file to a credentials file
        in the format expected by AWS tools (as described at http://j.mp/qGGHNp).
+      
+      see http://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html#cli-config-files
        
-       This is a port of the Perl code in SES.pm, so should behave identically."""
+       This is a port of the Perl code in SES.pm, so should behave identically.
+       
+      Note: Only handles one AWS profile (ie 'default' credentials) other credential configs will fail 
+
+       """
     
     result = {}
-    
+
     for line in fp:
 
         line = line.strip()
         match = LINE_RE.match(line)
+        print line
 
         if not line:
             continue
+        if '[default]' in line:
+            continue
         elif not match:
             raise ValueError, 'Cannot parse credentials file entry <%s>' % line
-        elif match.group(1) == 'AWSAccessKeyId':
+        elif match.group(1) == 'AWSAccessKeyId' or match.group(1) == 'aws_access_key_id':
             result['aws_access_key_id'] = match.group(2)
-        elif match.group(1) == 'AWSSecretKey':
+        elif match.group(1) == 'AWSSecretKey' or match.group(1) == 'aws_secret_access_key':
             result['aws_secret_access_key'] = match.group(2)
         else:
             raise ValueError, 'Unrecognized credential <%s>' % match.group(1)
-    
+
     return result
 
 
